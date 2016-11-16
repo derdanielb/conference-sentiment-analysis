0c7df2b3c05363539bad7bc257982123bc6a30d0
Ludwig, Sven <Sven.Ludwig@senacor.com>
2016-11-16 22:03:42 +0100

    rename application class

---
+++ csa-conference/src/main/java/net/csa/conference/ConferenceMicroservice.java
@@ -0,0 +1,13 @@
+package net.csa.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ConferenceMicroservice {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceMicroservice.class, args);
+    }
+
+}
