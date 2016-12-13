f3e4ec59c87614f00ddc6f63bcc0fac610839517
Mike <zellermike@icloud.com>
2016-12-13 16:02:44 +0100

    rest get ...

--- csa-conference/src/main/java/net/csa/conference/rest/ConferenceController.java
+++ csa-conference/src/main/java/net/csa/conference/rest/ConferenceController.java
@@ -2,7 +2,11 @@ package net.csa.conference.rest;
 
 import net.csa.conference.model.Conference;
 import net.csa.conference.repository.ConferenceRepository;
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.inject.Inject;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+import static net.csa.conference.rest.ErrorGenerator.createError;
+import static net.csa.conference.rest.ErrorGenerator.createNotFound;
+import static net.csa.conference.rest.ErrorGenerator.createServerError;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Created by Felix on 13.12.2016.
  */
 @RestController
-@RequestMapping("/confApi")
+@RequestMapping("/csa/v1")
 public class ConferenceController {
     private static final Logger log = getLogger(ConferenceController.class);
 
@@ -31,16 +38,30 @@ public class ConferenceController {
         this.repository = repository;
     }
 
-    @RequestMapping(path = "/conference", method = RequestMethod.GET)
-    public List<Conference> listConferences(){
+    @RequestMapping(path = "/conferences", method = RequestMethod.GET)
+    public ResponseEntity<?> listConferences(){
         try {
             List<Conference> target = new ArrayList<>();
             for (Conference c : repository.findAll().get())
                 target.add(c);
-            return target;
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return new ArrayList<>();
+            return ResponseEntity.ok(target);
+        } catch (Throwable t) {
+            return createServerError(t);
+        }
+    }
+
+    @RequestMapping(path = "/conferences/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getConference(@PathVariable String id){
+        try {
+            Conference c = repository.findOne(UUID.fromString(id)).get();
+            if(c != null)
+                return ResponseEntity.ok(c);
+            else
+                return createNotFound(id);
+        } catch (IllegalArgumentException e) {
+            return createError("Error: Invalid conference id: " + id);
+        } catch (Throwable t) {
+            return createServerError(t);
         }
     }
 }
---
+++ csa-conference/src/main/java/net/csa/conference/rest/ErrorGenerator.java
@@ -0,0 +1,40 @@
+package net.csa.conference.rest;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+/**
+ * Created by zelle on 13.12.2016.
+ */
+public class ErrorGenerator {
+    public static class Error{
+        public String error;
+        public int code;
+    }
+
+    public static ResponseEntity<Error> createError(String error, int code){
+        Error e = new Error();
+        e.code = code;
+        e.error = error;
+        return ResponseEntity.status(code).body(e);
+    }
+
+    public static ResponseEntity<Error> createError(String error, HttpStatus statusCode){
+        return createError(error, statusCode.value());
+    }
+
+    public static ResponseEntity<Error> createError(String error){
+        return createError(error, HttpStatus.BAD_REQUEST);
+    }
+
+    public static ResponseEntity<Error> createNotFound(String resource){
+        return createError("Error: Resource not found: " + resource, HttpStatus.NOT_FOUND);
+    }
+
+    public static ResponseEntity<Error> createServerError(Throwable throwable){
+        return createError(throwable.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
