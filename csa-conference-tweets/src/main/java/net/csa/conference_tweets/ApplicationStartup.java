fc63ba09c31101c09173e46432fbb35b9bb607f1
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-02-20 17:10:04 +0100

    added conference-tweets

---
+++ csa-conference-tweets/src/main/java/net/csa/conference_tweets/ApplicationStartup.java
@@ -0,0 +1,13 @@
+package net.csa.conference_tweets;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+    }
+}
---
+++ csa-conference-tweets/src/main/java/net/csa/conference_tweets/ConferenceTweetsMicroservice.java
@@ -0,0 +1,15 @@
+package net.csa.conference_tweets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceTweetsMicroservice {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceTweetsMicroservice.class, args);
+    }
+
+
+}
