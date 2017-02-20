47693ce990c34901da9dc978c59b720bf2365e8d
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-02-20 12:59:12 +0100

    extracted test data generation

---
+++ csa-conference/src/test/java/net/csa/conference/TestDataGenerator.java
@@ -0,0 +1,43 @@
+package net.csa.conference;
+
+import net.csa.conference.model.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by sky on 20.02.17.
+ */
+public class TestDataGenerator {
+    public static Conference createConference() {
+        Conference c = new Conference();
+        c.generateUUID();
+        c.setHashTag("baum");
+        c.setName("Baum");
+        List<Persona> orcs = new ArrayList<>();
+        orcs.add(new Organisation("o1"));
+        orcs.add(new Group("g1"));
+        orcs.add(new Person("Ralf", "Baum"));
+        c.setOrganisers(orcs);
+        List<Persona> spon = new ArrayList<>();
+        spon.add(new Organisation("o2"));
+        spon.add(new Group("g2"));
+        spon.add(new Person("Rolf", "B42"));
+        c.setSponsors(spon);
+        c.setTimeSpan(new TimeSpan(new Date(2016, 1, 1),
+                new Date(2016, 1, 2)));
+        EventLocation el = new EventLocation();
+        el.setName("Loc");
+        el.setGeoLocation(new Location(42, 42));
+        Address a = new Address();
+        a.setCountry("DE");
+        a.setNumber(42);
+        a.setStreet("BaumStreet");
+        a.setTown("BaumCity");
+        a.setZipCode(424242);
+        el.setAddress(a);
+        c.setLocation(el);
+        return c;
+    }
+}
--- csa-conference/src/test/java/net/csa/conference/repository/ConferenceRepositoryTest.java
+++ csa-conference/src/test/java/net/csa/conference/repository/ConferenceRepositoryTest.java
@@ -2,6 +2,7 @@ package net.csa.conference.repository;
 
 import net.csa.conference.model.*;
 
+import static net.csa.conference.TestDataGenerator.createConference;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.*;
@@ -242,36 +243,5 @@ public class ConferenceRepositoryTest {
         } catch (InterruptedException | ExecutionException e) {
             fail(e.getMessage());
         }
-    }
-
-    private static Conference createConference() {
-        Conference c = new Conference();
-        c.generateUUID();
-        c.setHashTag("baum");
-        c.setName("Baum");
-        List<Persona> orcs = new ArrayList<>();
-        orcs.add(new Organisation("o1"));
-        orcs.add(new Group("g1"));
-        orcs.add(new Person("Ralf", "Baum"));
-        c.setOrganisers(orcs);
-        List<Persona> spon = new ArrayList<>();
-        spon.add(new Organisation("o2"));
-        spon.add(new Group("g2"));
-        spon.add(new Person("Rolf", "B42"));
-        c.setSponsors(spon);
-        c.setTimeSpan(new TimeSpan(new Date(2016, 1, 1),
-                new Date(2016, 1, 2)));
-        EventLocation el = new EventLocation();
-        el.setName("Loc");
-        el.setGeoLocation(new Location(42, 42));
-        Address a = new Address();
-        a.setCountry("DE");
-        a.setNumber(42);
-        a.setStreet("BaumStreet");
-        a.setTown("BaumCity");
-        a.setZipCode(424242);
-        el.setAddress(a);
-        c.setLocation(el);
-        return c;
     }
 }
