ce828078486d48763b658eb078eb6663bde57350
Mike <zellermike@icloud.com>
2017-03-02 16:22:21 +0100

    fix Persona.fromList

--- csa-spa/src/app/conference.ts
+++ csa-spa/src/app/conference.ts
@@ -2,6 +2,9 @@ import {UUID} from "angular2-uuid";
 import {EventLocation} from "./event-location";
 import {Persona} from "./persona";
 import {TimeSpan} from "./time-span";
+import {Person} from "./person";
+import {Group} from "./group";
+import {Organisation} from "./organisation";
 
 export class Conference {
   public uuid : string = UUID.UUID();
@@ -18,8 +21,8 @@ export class Conference {
     this.timeSpan = new TimeSpan(obj.timeSpan);
     this.location = new EventLocation(obj.location);
     this.hashTag = obj.hashTag;
-    this.organisers = Persona.fromList(obj.organisers);
-    this.sponsors = Persona.fromList(obj.sponsors);
+    this.organisers = Conference.fromList(obj.organisers);
+    this.sponsors = Conference.fromList(obj.sponsors);
   }
 
   public toString() : string {
@@ -29,5 +32,25 @@ export class Conference {
       "\nHashTag: " + this.hashTag +
       "\nOrganisers: " + this.organisers +
       "\nSponsors: " + this.sponsors;
+  }
+
+  public static fromList(obj) : Persona[] {
+    var list : Persona[] = [];
+
+    obj.forEach(item => {
+      switch (item["@class"]){
+        case "net.csa.conference.model.Person":
+          list.push(new Person(item));
+          break;
+        case "net.csa.conference.model.Group":
+         list.push(new Group(item));
+         break;
+         case "net.csa.conference.model.Organisation":
+         list.push(new Organisation(item));
+         break;
+      }
+    });
+
+    return list;
   }
 }
--- csa-spa/src/app/persona.ts
+++ csa-spa/src/app/persona.ts
@@ -1,7 +1,3 @@
-import {Person} from "./person";
-import {Group} from "./group";
-import {Organisation} from "./organisation";
-
 export class Persona {
   public name : string;
 
@@ -9,25 +5,6 @@ export class Persona {
     this.name = obj.name;
   }
 
-  public static fromList(obj) : Persona[] {
-    var list : Persona[] = [];
-
-    JSON.parse(obj.text()).forEach(item => {
-      switch (item["@class"]){
-        case "Person":
-          list.push(new Person(item));
-          break;
-        case "Group":
-          list.push(new Group(item));
-          break;
-        case "Organisation":
-          list.push(new Organisation(item));
-          break;
-      }
-    });
-
-    return list;
-  }
   public toString() : string {
     return this.name;
   }
