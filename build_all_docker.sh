f36c7b3d16e2ab5ec0f3bb3a7f2e95d4923a8865
Mike <zellermike@icloud.com>
2017-03-02 18:41:37 +0100

    .

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -9,3 +9,10 @@ echo "csa-conference-tweets done."
 cd ../csa-twitter-search
 ../docker_image.sh
 echo "csa-twitter-search done."
+
+cd ../csa-spa
+./build_spa.sh
+echo "csa-spa done."
+
+cd ../csa-docker-composition
+sudo docker-compose up -d
